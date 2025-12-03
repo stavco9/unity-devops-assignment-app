c87da94564a253afa5dcaf0ffb614988b110e26b
Stav Cohen <stavco9@gmail.com>
2025-12-03 17:42:08 +0200

    Add hello world apps

---
+++ web-server/src/index.ts
@@ -0,0 +1,13 @@
+// index.ts
+import express from "express";
+const app = express();
+const port = "3000";
+
+app.get("/", (req, res) => {
+  res.send("Hello World from web-server!");
+  console.log("Response sent");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
